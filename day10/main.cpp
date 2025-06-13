9c0cab0816feb2302344a4307fd57c8d01b21c3a
wanghao76 <wanghao76@lenovo.com>
2025-06-13 11:35:16 +0800

    update

---
+++ day10/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    // This is a placeholder for the main function.
+    // You can add your code here to implement the functionality you need.
+    return 0;
+}
