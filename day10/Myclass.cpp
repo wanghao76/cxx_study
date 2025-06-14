8d5b36b2e93e1a95a0f7b88b728dc88c92994323
wanghao76 <wh1985876096@qq.com>
2025-06-14 18:28:37 +0800

    update1

--- day10/Myclass.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <algorithm>
-#include <ostream>
-#include "Myclass.h"
-
-std::ostream& operator<<(std::ostream& os, const MyClass& obj) {
-    os << "(" << obj.get_x() << ", " << obj.get_y() << ")";
-    return os;
-}
